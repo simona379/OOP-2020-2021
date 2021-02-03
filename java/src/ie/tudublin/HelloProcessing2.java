a5cd41c99899c981925fdb6a4a4b711950d0fee6
Simona Petrauskaite <simonapetrauskaite@Simonas-MacBook-Pro.local>
2021-02-03 16:17:13 +0000

    my attempt

--- java/src/ie/tudublin/HelloProcessing2.java
+++ java/src/ie/tudublin/HelloProcessing2.java
@@ -1,23 +1,55 @@
 package ie.tudublin;
 
+import ddf.minim.analysis.TriangularWindow;
 import processing.core.PApplet;
 
 public class HelloProcessing2 extends PApplet
 {
 
+	//first code to be executed
 	public void settings()
 	{
 		size(500, 500);
 	}
-
+	//second method to get excecuted
 	public void setup() {
 		
 	}
-	
+	//draw gets called 60 times a second
 	public void draw()
 	{	
-		background(0);
-		stroke(255);
-		line(10, 10, 100, 100);
+		background(255, 0, 0);
+
+		noStroke();	
+		fill(255, 255, 0);
+		ellipse(250, 350, 300, 300);
+
+		fill(51, 204, 255);
+		triangle(120, 345, 260, 90, 400, 345);
+
+		/* background(0); //black
+		stroke(255); //pen coloour
+		//stroke(0, 255, 0);
+		line(10, 10, 100, 100); //x1, y1, x2, y2
+		//line(); //four parameters
+
+		noStroke();	
+
+		//circle
+		ellipse(200,200, 100, 50); //cx, cy, w, h
+
+
+		fill(0, 0, 255);
+		//rectangle
+		rect(20, 100, 70, 90); //
+		point(200, 60);
+
+		fill(0, 255, 255);
+		triangle(200, 90, 300, 200, 10, 60);
+
+		//draw text to screen
+		fill(0);
+		text("Hello World!", 300, 300);
+ */
 	}
 }
